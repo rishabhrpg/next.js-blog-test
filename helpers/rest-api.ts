166296d1c7cac953208305130cfe047a79004bd9
Rishabh <r.gausai@gmail.com>
2022-12-10 22:06:52 +0530

    refactor. extracted reusable rest-api helper class

---
+++ helpers/rest-api.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export class RestAPI {
+
+    private availableMethods: string[] = [];
+
+    constructor(private req: NextApiRequest, private res: NextApiResponse) { }
+
+    public set onGet(callback) {
+        if (this.req.method === 'GET') {
+            this.availableMethods.push(this.req.method);
+            callback();
+        }
+    }
+
+    public set onPost(callback) {
+        if (this.req.method === 'POST') {
+            this.availableMethods.push(this.req.method);
+            callback();
+        }
+    }
+
+    public set onDefault(callback) {
+        if (!this.availableMethods.includes(this.req.method)) {
+           callback();
+        }
+    }
+
+    public get onDefault() {
+        if (!this.availableMethods.includes(this.req.method)) {
+            this.res.setHeader('Allow', this.availableMethods);
+            this.res.status(405).end(`Method ${this.req.method} Not Allowed`);
+        }
+        return () => {};
+    }
+
+    public get onGet() {
+        return this.unreadableError({ method: 'onGet' });
+    }
+
+    public get onPost() {
+        return this.unreadableError({ method: 'onPost' });
+    }
+
+    unreadableError({ method }: { method: string }) {
+        throw new Error(`${method} is not callable, please assign a new function instead`);
+        return () => { };
+    }
+}
--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,54 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-export class RestAPI {
-
-    private availableMethods: string[] = [];
-
-    constructor(private req: NextApiRequest, private res: NextApiResponse) { }
-
-    public set onGet(callback) {
-        if (this.req.method === 'GET') {
-            this.availableMethods.push(this.req.method);
-            callback();
-        }
-    }
-
-    public set onPost(callback) {
-        if (this.req.method === 'POST') {
-            this.availableMethods.push(this.req.method);
-            callback();
-        }
-    }
-
-    public set onDefault(callback) {
-        if (!this.availableMethods.includes(this.req.method)) {
-           callback();
-        }
-    }
-
-    public get onDefault() {
-        if (!this.availableMethods.includes(this.req.method)) {
-            this.res.setHeader('Allow', this.availableMethods);
-            this.res.status(405).end(`Method ${this.req.method} Not Allowed`);
-        }
-        return () => {};
-    }
-
-    public get onGet() {
-        return this.unreadableError({ method: 'onGet' });
-    }
-
-    public get onPost() {
-        return this.unreadableError({ method: 'onPost' });
-    }
-
-    unreadableError({ method }: { method: string }) {
-        throw new Error(`${method} is not callable, please assign a new function instead`);
-        return () => { };
-    }
-}
-
-
+import { RestAPI } from "../../helpers/rest-api";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const {
@@ -70,4 +21,3 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     api.onDefault();
 }
-
